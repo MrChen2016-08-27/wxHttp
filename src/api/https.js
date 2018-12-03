5afe1689184e7c54d1700c14e1d7099c5d7c16cc
1909916440@qq.comm <1909916440@qq.com>
2018-12-03 18:06:04 +0800

    完善范

--- src/api/https.js
+++ src/api/https.js
@@ -1,106 +1,102 @@
 const resHandler = {
     success: (response, reqUrl) => {
-        return response;
+        return response
     },
-    error: (err) => {
-        console.log(`ajax错误:${JSON.stringify(err)}`);
-        return err;
+    error: err => {
+        console.log(`ajax错误:${JSON.stringify(err)}`)
+        return err
     }
-};
+}
 
 const reqHandler = {
-    before: (reqUrl) => {
-
-    },
-};
-
-
+    before: reqUrl => {}
+}
 
 class Https {
-    constructor() {
-        this.type = null;
-        this.data =  { r: Date.now() };
+    constructor () {
+        this.type = null
+        this.data = { r: Date.now() }
         this.defaults = {
             baseUrl: '',
             headers: {
-                'content-type': 'application/json' // 默认值
-            },
-        };
+                'content-type': 'application/json', // 默认值
+                'Authorazition': 'Bearer token'
+            }
+        }
 
         this.interceptors = {
             response: {
                 use: function (success, error) {
                     if (success) {
-                        resHandler.success = success;
+                        resHandler.success = success
                     }
                     if (error) {
-                        resHandler.error = error;
+                        resHandler.error = error
                     }
                 }
             },
             request: {
                 use: function (before) {
                     if (before) {
-                        reqHandler.before = before;
+                        reqHandler.before = before
                     }
                 }
             }
-        };
+        }
     }
-    
-    request({ url, type, data, header }) {
+
+    request ({ url, type, data, header }) {
+        console.log('wx request')
         return new Promise((resolve, reject) => {
-            let reqUrl = `${this.defaults.baseUrl}${url}`;
-            reqHandler.before(reqUrl);
+            let reqUrl = `${this.defaults.baseUrl}${url}`
+            reqHandler.before(reqUrl)
             wx.request({
                 url: reqUrl,
                 method: type || this.type,
                 data: {
                     ...this.data,
-                    ...data,
+                    ...data
                 },
                 header: Object.assign(this.defaults.headers, header),
                 success: function (res) {
-                    let response = resHandler.success(res, reqUrl);
-                    resolve(response);
+                    let response = resHandler.success(res, reqUrl)
+                    resolve(response)
                 },
                 fail: function (err) {
                     let errObj = resHandler.error(err)
-                    reject(errObj);
+                    reject(errObj)
                 }
-            });
-            console.log(this.data, '***');
-
-        });
+            })
+            console.log(this.data, '***')
+        })
     }
     /**
      * @desc 文件上传接口
-     * @params 
+     * @params
      */
-    upload({ url, filePath, name }) {
+    upload ({ url, filePath, name }) {
         return new Promise((resolve, reject) => {
-            reqHandler.before();
+            reqHandler.before()
             wx.uploadFile({
                 url: this.defaults.baseUrl + url,
                 filePath,
                 name,
                 success (res) {
-                    let response = resHandler.success(res);
-                    resolve(response);
+                    let response = resHandler.success(res)
+                    resolve(response)
                 }
             })
-        });
+        })
     }
 
-    static getInstance() {
+    static getInstance () {
         if (Https.instance) {
-            return Https.instance;
+            return Https.instance
         } else {
-            Https.instance = new Https();
-            return Https.instance;
+            Https.instance = new Https()
+            return Https.instance
         }
     }
 }
 
-
-module.exports = Https.getInstance();
+export default Https.getInstance()
